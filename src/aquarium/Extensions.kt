5ee0fd04c7493004ef60cbf984877ff9e833000c
bjs3du <bshelton906@gmail.com>
2020-04-27 13:32:32 -0400

    update

--- src/aquarium/Extensions.kt
+++ src/aquarium/Extensions.kt
@@ -1,1 +1,41 @@
 package aquarium
+
+fun String.hasSpaces() = find {it == ' '} != null
+
+fun extensionsExample() {
+    "Does it have spaces?".hasSpaces()
+}
+
+open class AquariumPlant(val color: String, private val size: Int)
+class GreenLeafyPlant(size: Int): AquariumPlant("Green", size)
+
+fun AquariumPlant.isRed() = color == "Red"
+
+fun AquariumPlant.print() = println("AquariumPlant")
+fun GreenLeafyPlant.print() = println("GreenLeafyPlant")
+
+fun staticExample() {
+    val plant = GreenLeafyPlant(size = 50)
+    plant.print()
+
+    val aquariumPlant: AquariumPlant = plant
+    aquariumPlant.print()
+}
+
+val AquariumPlant.isGreen: Boolean
+get() = color == "Green"
+
+fun propertyExample() {
+    val plant = AquariumPlant("Green", 50)
+    plant.isGreen
+}
+
+fun AquariumPlant?.pull() {
+
+    this?.apply { println("removing $this") }
+}
+
+fun nullableExample() {
+    val plant: AquariumPlant? = null
+    plant.pull()
+}
--- src/book2/BookTwo.kt
+++ src/book2/BookTwo.kt
@@ -1,6 +1,8 @@
 package book2
 
-class Book(val title: String, val author: String, val year: Int) {
+import java.util.*
+
+class Book(val title: String, val author: String, val year: Int, var pages: Int = 0) {
 
     fun getTitleAuthor(): Pair<String, String> {
         return (title to author)
@@ -13,7 +15,6 @@ class Book(val title: String, val author: String, val year: Int) {
     fun canBorrow(hasBooks: Int): Boolean {
         return (hasBooks < MAX_NUMBER_BOOKS)
     }
-
 
 
     object Constants {
@@ -29,36 +30,56 @@ class Book(val title: String, val author: String, val year: Int) {
         val BASE_URL = "http://www.turtlecare.net/"
     }
 
-    val pages : Int? = 0
+
 }
 
 const val MAX_NUMBER_BOOKS = 20
 
+fun Book.weight(): Double {
+    return (pages * 1.5)
+}
+
+fun Book.tornPages(torn: Int) = if (pages >= torn) pages -= torn else pages = 0
+
+class Puppy() {
+    fun playWithBook(book: Book) {
+        book.tornPages(Random().nextInt(12))
 
 
-fun main(args: Array<String>) {
-
-    val book = Book("Romeo and Juliet", "William Shakespeare", 1597)
-    val bookTitleAuthor = book.getTitleAuthor()
-    val bookTitleAuthorYear = book.getTitleAuthorYear()
-
-    println("Here is your book ${bookTitleAuthor.first} by ${bookTitleAuthor.second}")
-
-    println("Here is your book ${bookTitleAuthorYear.first} " +
-            "by ${bookTitleAuthorYear.second} written in ${bookTitleAuthorYear.third}")
-
-    val allBooks = setOf("Macbeth", "Romeo and Juliet", "Hamlet", "A Midsummer Night's Dream")
-    val library = mapOf("Shakespeare" to allBooks)
-    println(library.any { it.value.contains("Hamlet") })
-
-    val moreBooks = mutableMapOf<String, String>("Wilhelm Tell" to "Schiller")
-    moreBooks.getOrPut("Jungle Book") { "Kipling" }
-    moreBooks.getOrPut("Hamlet") { "Shakespeare" }
-    println(moreBooks)
-
-
-
-
+    }
 }
 
 
+    fun main(args: Array<String>) {
+
+        val book = Book("Romeo and Juliet", "William Shakespeare", 1597)
+        val bookTitleAuthor = book.getTitleAuthor()
+        val bookTitleAuthorYear = book.getTitleAuthorYear()
+
+        println("Here is your book ${bookTitleAuthor.first} by ${bookTitleAuthor.second}")
+
+        println(
+            "Here is your book ${bookTitleAuthorYear.first} " +
+                    "by ${bookTitleAuthorYear.second} written in ${bookTitleAuthorYear.third}"
+        )
+
+        val allBooks = setOf("Macbeth", "Romeo and Juliet", "Hamlet", "A Midsummer Night's Dream")
+        val library = mapOf("Shakespeare" to allBooks)
+        println(library.any { it.value.contains("Hamlet") })
+
+        val moreBooks = mutableMapOf<String, String>("Wilhelm Tell" to "Schiller")
+        moreBooks.getOrPut("Jungle Book") { "Kipling" }
+        moreBooks.getOrPut("Hamlet") { "Shakespeare" }
+        println(moreBooks)
+
+
+        val puppy = Puppy()
+        val book2 = Book("Oliver Twist", "Charles Dickens", 1837, 540)
+
+        puppy.playWithBook(book2)
+        while (book2.pages > 0) {
+            puppy.playWithBook(book2)
+            println("${book2.pages} left in ${book2.title}")
+        }
+        println("Sad puppy, no more pages in ${book2.title}. ")
+    }
