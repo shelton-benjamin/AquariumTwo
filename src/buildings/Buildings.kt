4a775f76e1afea11a6574742999ab76881314fb1
bjs3du <bshelton906@gmail.com>
2020-04-28 10:10:36 -0400

    update

---
+++ src/buildings/Buildings.kt
@@ -0,0 +1,36 @@
+package buildings
+
+fun main() {
+
+    Building(Wood()).build()
+    isSmallBuilding(Building(Brick()))
+}
+
+open class BaseBuildingMaterial() {
+
+    open val numberNeeded = 1
+}
+
+class Wood : BaseBuildingMaterial() {
+    override val numberNeeded: Int
+        get() = 4
+}
+
+class Brick : BaseBuildingMaterial() {
+    override val numberNeeded: Int
+        get() = 8
+}
+
+class Building<out T : BaseBuildingMaterial>(val buildingMaterial: T) {
+    val baseMaterialsNeeded = 100
+    val actualMaterialsNeeded = buildingMaterial.numberNeeded * baseMaterialsNeeded
+
+    fun build() {
+        println(" $actualMaterialsNeeded ${buildingMaterial::class.simpleName} required")
+    }
+}
+
+fun <T : BaseBuildingMaterial> isSmallBuilding(building: Building<T>) {
+    if (building.actualMaterialsNeeded < 500) println("Small building")
+    else println("large building")
+}
